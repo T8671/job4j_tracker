3922909c2c0005a5b24718704d0c55e550f457b8
Teymur <azimovta@gmail.com>
2024-02-25 17:56:27 +0500

    6. Правила Checkstyle [#504981 #483041]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -1,9 +1,11 @@
 package ru.job4j.checkstyle;
 
 public class Broken {
-    public String surname;
+
     public static final String NEW_VALUE = "";
+    public String surname;
     String name;
+    private int sizeOfEmpty = 10;
 
     Broken() {
     }
@@ -19,6 +21,4 @@ public class Broken {
 
     void method(int a, int b, int c, int d, int e, int f, int g) {
     }
-
-    private int sizeOfEmpty = 10;
 }
