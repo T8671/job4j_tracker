2d2016955ce038a744c1597b11d4bd8d0daef5c4
Teymur <azimovta@gmail.com>
2024-02-28 20:45:56 +0500

    Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,20 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
     private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
 
     public int getId() {
         return id;
