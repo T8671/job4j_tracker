2cc87919d2b4172956222268b725c5b868e1bc9a
Teymur <azimovta@gmail.com>
2024-07-08 23:52:29 +0500

    0. Map, HashMap [#212652 #504108]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,10 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("parsentev@yandex.ru", "Petr Arsentev Sergeevich");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("parsentev1@yandex.ru", "Petr Velikiy");
+        map.put("parsentev2@yandex.ru", "Petr 1");
+        map.put("parsentev3@yandex.ru", "Petr Ivanovich");
 
         for (String key : map.keySet()) {
             System.out.println(map.get(key));
